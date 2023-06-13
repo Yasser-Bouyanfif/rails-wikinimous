a45c6c62c2551c22c49629ef3e9499a1918bcefb
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-06-13 16:28:22 +0200

    exo3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: 'articles#index'
+  # post 'articles', to: 'articles#create'
+  # get 'articles/new', to: 'articles#new'
+  # get 'articles/:id', to: 'articles#show', as: :article
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  # patch 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
+  resources :articles
+  root to: 'articles#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do
+  Article.create(
+    title: Faker::Sports::Football.team,
+    content: Faker::Sports::Football.player
+  )
+end
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
